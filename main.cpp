be7601d85e1b6bb3382aabfff741378b04752c76
HugocamposL3 <75391487+HugocamposL3@users.noreply.github.com>
2023-02-14 16:11:11 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -14,17 +14,17 @@ PwmOut   MA2 (D6);
 PwmOut   MB1 (D9);
 PwmOut   MB2 (D10);
 
-AnalogIn S1 (A0);
-AnalogIn S2 (A1);
+AnalogIn S0 (A0);
+AnalogIn S1 (A1);
 
 DigitalIn bt (PC_13);
 // Blinking rate in milliseconds
 #define BLINKING_RATE     500ms
 
+int s0_value;
 int s1_value;
-int s2_value;
 
-float pwm = 0.2;
+//Serial pc(USBTX, USBRX); // tx, rx
 
 int main()
 {
@@ -37,36 +37,43 @@ int main()
     MA2.period(0.020);
     MB1.period(0.020);
     MB2.period(0.020);
-    
+    float pwm = 0.2;
 
     while (true) {
-        s0_value = S1.read()*1000;
-        s1_value = S2.read()*1000;
+        s0_value = S0.read()*1000;
+        s1_value = S1.read()*1000;
         /*
+        printf("Valor S0= %d\n\r", s0_value);
         printf("Valor S1= %d\n\r", s1_value);
-        printf("Valor S2= %d\n\r", s2_value);
         ThisThread::sleep_for(BLINKING_RATE);
         */
         // Frente
-        if ((s1_value<200) && (s2_value<200)){
+        if ((s0_value<200) && (s1_value<200)){
             MA1 = pwm;
             MA2 = 0;
             MB1 = pwm;
             MB2 = 0;
         }
         //Esquerda
-        if ((s1_value<200) && (s2_value>200)){
+        if ((s0_value<200) && (s1_value>200)){
             MA1 = 0;
             MA2 = 0;
             MB1 = pwm;
             MB2 = 0;
         }
         //Direita
-        if ((s1_value>200) && (s2_value<200)){
+        if ((s0_value>200) && (s1_value<200)){
             MA1 = pwm;
             MA2 = 0;
             MB1 = 0;
             MB2 = 0;
         }
+        //Parar
+        if((s0_value>200) && (s1_value>200)){
+            MA1 = 0;
+            MA2 = 0;
+            MB1 = 0;
+            MB2 = 0;
+        }   
     }
 }
